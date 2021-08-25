5484e4087da32c654bcb2dc45cff767bcc5a44b0
erickhartawan <hartawanerick@gmail.com>
2021-08-25 12:29:18 +1000

    about us page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.sass";
 import Navbar from "./components/layouts/Navbar";
-import Home from "./components/pages/Home";
+import Home from "./components/pages/Home/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AboutUs from "./components/pages/AboutUs/AboutUs";
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <Home />
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/aboutus" component={AboutUs} />
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
