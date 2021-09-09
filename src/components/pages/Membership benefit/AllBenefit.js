a1ccf17f16da6ed38730054b526383cd55f4ee4a
erickhartawan <hartawanerick@gmail.com>
2021-09-09 19:59:33 +1000

    changed All benefit page to use map

--- src/components/pages/Membership benefit/AllBenefit.js
+++ src/components/pages/Membership benefit/AllBenefit.js
@@ -1,111 +1,43 @@
-import "./AllBenefit.sass"
+import "./AllBenefit.sass";
 
-function AllBenefit(){
-    return(
+function AllBenefit() {
+    let data = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "z",
+        "q",
+        "x",
+        "y",
+        "g",
+    ];
+    return (
         <>
-        <div className = "allBenefit">
-            <div className = "card-container-2">
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
+            <div className="allBenefit">
+                <div className="card-container-2">
+                    {data.map((item) => {
+                        return (
+                            <div className="card">
+                                <div className="grey-block-small"></div>
+                                <div className="store-small">store</div>
+                                <div className="promo-small">promo</div>
+                                <div className="tc-small">{item}</div>
+                            </div>
+                        );
+                    })}
                 </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-                <div className = "card">
-                    <div className = "grey-block-small"></div>
-                    <div className = "store-small">store</div>
-                    <div className = "promo-small">promo</div>
-                    <div className = "tc-small">tc</div>
-                </div>
-
             </div>
-        </div>
         </>
-    )
+    );
 }
 
 export default AllBenefit;
